db97759c608b243a184f457c42d38f9960704fae
Bartek Andree <andree.bartek@gmail.com>
2023-07-10 14:20:48 +0200

    second commit

--- src/main/java/com/deloitte/ads/App.java
+++ src/main/java/com/deloitte/ads/App.java
@@ -2,12 +2,9 @@ package com.deloitte.ads;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Mario!");
     }
 }
