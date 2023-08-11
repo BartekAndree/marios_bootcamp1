99cfe59e7eb5428c21dc32ceb5bc45773833f0a2
BartekAndree <andree.bartek@gmail.com>
2023-08-11 16:00:27 +0200

    adds a working keycloak configuration

---
+++ src/main/java/com/deloitte/ads/marios/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.deloitte.ads.marios.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JwtConverter jwtConverter;
+
+    public SecurityConfig(JwtConverter jwtConverter) {
+        this.jwtConverter = jwtConverter;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/user/all")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtConverter);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+}
